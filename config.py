07ff6995222c3e7525a03e4db8177a33ee2f64bb
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-11-29 03:34:16 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,85 +1,77 @@
-# (©)CodeXBotz
-
 import os
 import logging
-from dotenv import load_dotenv
 from logging.handlers import RotatingFileHandler
-import pymongo
 
-load_dotenv()
 
-# MongoDB Connection
-DB_URI = os.getenv("DATABASE_URL", "mongodb+srv://ultroidxTeam:ultroidxTeam@cluster0.gabxs6m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.getenv("DATABASE_NAME", "config_database")
-client = pymongo.MongoClient(DB_URI)
-db = client[DB_NAME]
-config_collection = db["bot_config"]
+#Bot token @Botfather
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
-DEFAULT_CONFIG = {
-    "TG_BOT_TOKEN": "801X1voG_YraNDCpUPYU515mKPJwdo64",
-    "APP_ID": 22505271,
-    "API_HASH": "c89a94fcfda4bc06524d0903977fc81e",
-    "DELETE_AFTER": 60,
-    "OWNER_ID": 6695586027,  # Replace with your ID
-    "CHANNEL_ID": -1002075726565,
-    "FORCE_SUB_CHANNEL": 0
-}
+LINK = os.environ.get("LINK","")
+CHAT = os.environ.get("CHAT","")
+CHANNEL = os.environ.get("CHANNEL","")
 
-def get_config(key, default=None):
-    config = config_collection.find_one({"key": key})
-    return config["value"] if config else DEFAULT_CONFIG.get(key, default)
+#Your API ID & API HASH from my.telegram.org [https://youtu.be/gZQJ-yTMkEo?si=H4NlUUgjsIc5btzH]
+#Your API ID from my.telegram.org
+APP_ID = int(os.environ.get("APP_ID", "22505271"))
 
-# Fetching variables dynamically
-TG_BOT_TOKEN = get_config("TG_BOT_TOKEN")
-APP_ID = int(get_config("APP_ID"))
-API_HASH = get_config("API_HASH")
-DELETE_AFTER = int(get_config("DELETE_AFTER"))
-OWNER_ID = int(get_config("OWNER_ID"))
+#Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "c89a94fcfda4bc06524d0903977fc81e")
 
-LINK = get_config("LINK")
-CHAT = get_config("CHAT")
-CHANNEL = get_config("CHANNEL")
+#Your db channel Id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002075726565"))
 
-CHANNEL_ID = int(get_config("CHANNEL_ID", 0))
-PAYMENT_QR = get_config("PAYMENT_QR")
-PAYMENT_TEXT = get_config("PAYMENT_TEXT")
+#OWNER ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "6695586027"))
 
-PORT = os.getenv("PORT", "8080")
+#Port
+PORT = os.environ.get("PORT", "8585")
 
-DB_URI = get_config("DATABASE_URL", DB_URI)
-DB_NAME = get_config("DATABASE_NAME", DB_NAME)
+#Database 
+#Database [https://youtu.be/qFB0cFqiyOM?si=fVicsCcRSmpuja1A]
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://ultroidxTeam:ultroidxTeam@cluster0.gabxs6m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
-FORCE_SUB_CHANNEL = int(get_config("FORCE_SUB_CHANNEL", 0))
+#Shortner (token system) 
+# check my discription to help by using my refer link of shareus.io
+# 
 
-TG_BOT_WORKERS = int(get_config("TG_BOT_WORKERS", "4"))
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "inshorturl.com")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "9f943360c339cec4fed66d9d5cbaa0c2b3d41f81")
+VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
+IS_VERIFY = os.environ.get("IS_VERIFY", "True")
+TUT_VID = os.environ.get("TUT_VID", "https://t.me/+PZfHvzjSiZc4OGE1") # shareus ka tut_vid he 
 
-START_PIC = get_config("START_PIC")
-START_MSG = get_config("START_MESSAGE", "Hello {first}\n\nI can store private files in a specified channel, and other users can access them via a special link.")
+#force sub channel id, if you want enable force sub
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002108419450"))
 
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+
+#start message
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
-    ADMINS = []
-    for x in get_config("ADMINS", "").split():
+    ADMINS=[]
+    for x in (os.environ.get("ADMINS", "1480923991 5069922547 6695586027").split()):
         ADMINS.append(int(x))
 except ValueError:
-    raise Exception("Your Admins list does not contain valid integers.")
+        raise Exception("Your Admins list does not contain valid integers.")
 
-FORCE_MSG = get_config("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join my Channel/Group to use me\n\nKindly join the Channel</b>")
+#Force sub message 
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
 
-CUSTOM_CAPTION = get_config("CUSTOM_CAPTION", None)
+#set your Custom Caption here, Keep None for Disable Custom Caption
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "This video/Photo/anything is available on the internet. We LeakHubd or its subsidiary channel doesn't produce any of them.")
 
-PROTECT_CONTENT = os.getenv('PROTECT_CONTENT', "False") == "True"
+#set True if you want to prevent users from forwarding files from bot
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
 
-AUTO_DELETE_TIME = int(get_config("AUTO_DELETE_TIME", "600"))
-AUTO_DELETE_MSG = get_config("AUTO_DELETE_MSG", "This file will be automatically deleted in {time} seconds. Please ensure you have saved any necessary content before this time.")
-AUTO_DEL_SUCCESS_MSG = get_config("AUTO_DEL_SUCCESS_MSG", "Your file has been successfully deleted. Thank you for using our service. ✅")
-
-DISABLE_CHANNEL_BUTTON = os.getenv("DISABLE_CHANNEL_BUTTON", None) == 'True'
+#Set true if you want Disable your Channel Posts Share button
+DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌ Don't send me messages directly. I'm only a File Share bot!"
+USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
+ADMINS.append(6695586027)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
@@ -97,6 +89,7 @@ logging.basicConfig(
     ]
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
