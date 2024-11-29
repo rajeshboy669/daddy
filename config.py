9c093e376674ebeeb71b5fe6e84aea7c43f64630
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-11-29 05:36:41 -0800

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv()
 START_PIC = os.environ.get("START_PIC","https://envs.sh/Ype.jpg")
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8011382713:AAGxzZX1voG_YraNDCpUPYU515mKPJwdo64")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8011382U515mKPJwdo64")
 
 LINK = os.environ.get("LINK","youtube.com/@phdlust")
 CHAT = os.environ.get("CHAT","uofficial")
