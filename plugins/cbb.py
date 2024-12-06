6fa38ba530895d20c8a08a0bda03a92ee6968bc9
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-12-06 09:59:03 -0800

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,28 +1,33 @@
-#(©)Codexbotz
+﻿# https://t.me/Ultroid_Official/524
 
-from pyrogram import __version__
-from bot import Bot
-from config import OWNER_ID
+from pyrogram import __version__, Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.enums import ParseMode
+from database.database import full_userbase
+from bot import Bot
+from config import OWNER_ID, ADMINS, CHANNEL, SUPPORT_GROUP, OWNER
 
+# Callback query handler
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
+
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
-            disable_web_page_preview = True,
-            reply_markup = InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
-                    ]
-                ]
+            text=f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n"
+                 f"○ Language : <code>Python3</code>\n"
+                 f"○ Library : <a href='https://Phdlust.co/'>Pyrogram asyncio {__version__}</a>\n"
+                 f"○ Source Code : <a href='https://youtu.be/BeNBEYc-q7Y'>Click here</a>\n"
+                 f"○ Channel : @{CHANNEL}\n"
+                 f"○ Support Group : @{SUPPORT_GROUP}</b>",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("🔒 Close", callback_data="close")]]
             )
         )
     elif data == "close":
         await query.message.delete()
         try:
             await query.message.reply_to_message.delete()
-        except:
-            pass
+        except Exception as e:
+            print(f"Error deleting reply-to message: {e}")
