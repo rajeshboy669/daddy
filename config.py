1cad65d98f24fa3c8710f7405996057575504107
Proloottricks <ritiksakhala17@gmail.com>
2024-12-06 23:01:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ AUTO_DELETE = os.environ.get("AUTO_DELETE", True) #ON/OFF
 GET_AGAIN = os.environ.get("GET_AGAIN", False) #ON/OFF
 DELETE_INFORM = os.environ.get("INFORM" , "Your file has been successfully deleted✅")
 NOTIFICATION = os.environ.get("NOTIFICATION" ,"This file will be automatically deleted in 20 minutes(Due to Copyright Issues).")
-GET_INFORM = os.environ.get("GET_INFORM" ,"File was deleted after {DELETE_AFTER} seconds. Use the button below to GET FILE AGAIN.")
+GET_INFORM = os.environ.get("GET_INFORM" ,"This file will be automatically deleted in 20 minutes(Due to Copyright Issues).")
 
 #Shortner (token system) 
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "inshorturl.com")
